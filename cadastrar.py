4870df6bb46d2c0df8a39f466d3d09766daa1545
Nicolas Damasceno <nicolassalaes204@gmail.com>
2023-12-07 10:52:23 -0300

    Sistema Principal com conexão 1.3

---
+++ cadastrar.py
@@ -0,0 +1,41 @@
+def cadastrarFuncionario():
+    print("Cadastrar Funcionário")
+    print("------------------------")
+    print()
+    setor = ''
+    while True:
+        nome = input('Digite nome completo do funcionário: ').strip()
+        if not nome:
+            print("É necessário digitar algum caractere para continuar!")
+        elif all(part.isalpha() or part.isspace() for part in nome.split()):
+            break
+        else:
+            print("Nesse campo é válido apenas letras!\nTente novamente")
+    while True:
+        funcao = input('Digite a função do funcionário: ').strip()
+        if not funcao:
+            print("É necessário digitar algum caractere para continuar!")
+        elif all(part.isalpha() or part.isspace() for part in funcao.split()):
+            break
+        else:
+            print("Nesse campo é válido apenas letras!\nTente novamente")
+    while True:
+        try:
+            escolha = int(input('Digite o setor do funcionário 1- [ADM] 2- [OPR] 3- [TI] 4- [LOG] 5- [RH]: '))
+            if escolha == 1:
+                setor == 'ADM'
+                break
+            elif escolha == 2:
+                setor = 'OPR'
+                break
+            elif escolha == 3:
+                setor = 'TI'
+                break
+            elif escolha == 4:
+                setor = 'LOG'
+                break
+            elif escolha == 5:
+                setor = 'RH'
+                break
+        except ValueError:
+            print("Nesse campo é válido apenas números!\nTente novamente")
