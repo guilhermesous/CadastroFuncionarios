cd964eff50f00761112a5be315cb14a93b0e22b9
Guilherme Sousa <142254512+guilhermesous@users.noreply.github.com>
2023-12-07 14:40:07 -0300

    Delete conex.py

--- conex.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-conectar = mysql.connector.connect(host = 'localhost',
-                                   database = 'cadastrofuncionarios',
-                                   user = '',
-                                   password = '')
-if conectar.is_connected():
-    print('Conexão realizado com sucesso!')
-else:
-    print('falha na conexão')
