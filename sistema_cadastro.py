80b254cc978f049317aed0ebb011ca2fce7132fb
Nicolas Damasceno <nicolassales204@gmail.com>
2023-12-09 17:56:58 -0300

    Cadastro de funcionarios, Nicolas

---
+++ sistema_cadastro.py
@@ -0,0 +1,41 @@
+import os
+import conexao
+import time
+import cadastrar
+conectar = conexao.conectar
+os.system('cls')
+
+
+def main():
+    print("Bem-Vindo ao Sistema de Cadastro de Funcionários 1.0v")
+    print("--------------------------------------------------------")
+    print()
+    while True:
+        try:
+            escolha = int(input(
+                '[1]- Para cadastrar um novo funcinário\n[2]- Para listar os funcionários cadastrados\n[3]- Para pesquisar um funcionário\n[4]- Para deletar um funcionário\n[0]- Para encerrar o sistema\n-> ').strip())
+            if escolha == 1:
+                cadastrar.cadastrarFuncionario()
+            elif escolha == 2:
+                pass
+            elif escolha == 3:
+                pass
+            elif escolha == 4:
+                pass
+            elif escolha == 0:
+                print("Encerrando o programa...")
+                time.sleep(1)
+                os.system('cls')
+                break
+            else:
+                print("Número inválido!\nTente novamente")
+                time.sleep(0.7)
+                os.system('cls')
+        except ValueError:
+            print('Nesse campo é válido apenas números!\nTente novamente')
+            time.sleep(0.7)
+            os.system('cls')
+
+
+if __name__ == '__main__':
+    main()
