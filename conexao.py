41eed6beddfefd054957bacb30b5e8e0c3749dba
guilherme <guilherme.sousathe@gmail.com>
2023-12-12 09:11:22 -0300

    Conexão com o Banco

--- conexao.py
+++ conexao.py
@@ -3,8 +3,8 @@ os.system ('cls')
 import mysql.connector
 conectar = mysql.connector.connect(host = 'localhost',
                                    database = 'cadastrofuncionarios',
-                                   user = 'guilherme',
-                                   password = '789456')
+                                   user = '',
+                                   password = '')
 if conectar.is_connected():
     print('Conexão realizado com sucesso!')
 else:
